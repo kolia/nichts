5b66f5ee392dab7fc61b60262a47bb877e2a7503
kolia <kolia.sadeghi@gmail.com>
2021-07-30 15:02:42 -0400

    moar

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -5,3 +5,9 @@ alias k 'kak -c global'
 set -gx LD_LIBRARY_PATH $LD_LIBRARY_PATH /run/current-system/sw/lib
 
 fish_vi_key_bindings
+
+if test -z (pgrep ssh-agent)
+   eval (ssh-agent -c) > /dev/null
+   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+   set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+end
