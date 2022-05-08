eee94b29e26c7290eb3291073b72edc53e3c4aab
kolia <kolia.sadeghi@gmail.com>
2022-05-08 13:20:08 -0400

    manjaro

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,13 +1,5 @@
-set -gx PATH /home/kolia/julia-1.6.0-beta1/bin $PATH /home/kolia/.cargo/bin
+if status is-interactive
+    # Commands to run in interactive sessions can go here
 
-alias k 'kak -c global'
-
-set -gx LD_LIBRARY_PATH $LD_LIBRARY_PATH /run/current-system/sw/lib
-
-fish_vi_key_bindings
-
-if test -z (pgrep ssh-agent)
-   eval (ssh-agent -c) > /dev/null
-   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-   set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+    starship init fish | source
 end
